503fffa07b0563eb95ae19eb6a1691f56244a90e
autumn31 <autumn.forever31@gmail.com>
2017-09-02 23:01:56 +0800

    Done square

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 //document.body.style = "background: black";
 // var images=[];
-debug = false;
+debug = true;
 drawtext = false;
+var curDateObj = new Date();
+var curYear = curDateObj.getFullYear();
+var curMonth = curDateObj.getMonth()+1;
 $("#access").click(function(){
     // $.get("https://api.instagram.com/oauth/authorize/?client_id=e30d30a12f984aeb861617214be657eb&redirect_uri=http://localhost&response_type=token", function(data, status){
     //     console.log(data);
@@ -30,8 +33,13 @@ $("#month").click(function(){
     }
     for (i=1;i<13;i++){
         var images = [];
-        get_insta_by_month(images,2016,i);
+        get_insta_by_month(images,curYear,i);
     }
+});
+
+$("#pics").click(function(){
+    $('#instafeed').empty();
+    get_pics(curYear,curMonth);
 });
 
 $('document').ready(function(){
@@ -68,8 +76,8 @@ function get_insta(){
         //template: '<a href="{{link}}"><img src="{{image}}" /></a>',
         resolution: "standard_resolution",
         sortBy: 'most-recent',
-        max_t: new Date(2016,12),
-        min_t: new Date(2015,11),
+        max_t: new Date(curYear,curMonth),
+        min_t: new Date(curYear-2,curMonth-1),
         limit: 50,
         mock: true,
         success: function(response) {
@@ -81,6 +89,7 @@ function get_insta(){
                 // console.log(response.data);
                 feed.next();
             }else{
+                console.log("hi")
                 images = feed._sortBy(images,'likes.count',false);
                 images = images.slice(0,12);
                 images.sort(function() {
@@ -108,7 +117,8 @@ function get_insta(){
 feed.run();
 }
 
-function get_insta_by_month(images,y,i){
+function get_insta_by_month(images,cury,i){
+    var y=cury;
     var sy=y;
     var si=i-1;
     if (i===1){
@@ -134,27 +144,30 @@ function get_insta_by_month(images,y,i){
             if (debug){
                 console.log('response of '+y+', '+i);
             }
+            images = images.concat(response.data);
             if (feed.hasNext()){
                 feed.next();
             }
-            images = images.concat(response.data);
-            if (y===2016&&images.length<8){
-                images = images.concat(response.data);
-                get_insta_by_month(images,2015,i);
-            }else{
-                //console.log(images);
-                feed._sortBy(images,'likes.count',false);
-                // var can = document.createElement('canvas');
-                // $('#instafeed').append(can);
-                if (debug){
-                    console.log('adding images of '+i);
-                }
-                if (images[0]===undefined){
-                    add_pic(document.getElementById('can'+i),'',i);
+            else {
+                if (y!=cury-2&&images.length<8){
+                    images = images.concat(response.data);
+                    y--;
+                    get_insta_by_month(images,y,i);
                 }else{
-                    add_pic(document.getElementById('can'+i),images[0],i);
+                    //console.log(images);
+                    feed._sortBy(images,'likes.count',false);
+                    // var can = document.createElement('canvas');
+                    // $('#instafeed').append(can);
+                    if (debug){
+                        console.log('adding images of '+i);
+                    }
+                    if (images[0]===undefined){
+                        add_pic(document.getElementById('can'+i),'',i);
+                    }else{
+                        add_pic(document.getElementById('can'+i),images[0],i);
+                    }
+                    
                 }
-                
             }
         },
         error: function(){
@@ -175,12 +188,58 @@ function get_insta_by_month(images,y,i){
     feed.run();
 }
 
+function get_pics(cury,i){
+    var y=cury;
+    var sy=y;
+    var si=i-1;
+    if (i===1){
+        sy=y-1;
+        si=12;
+    }
+    var token = location.href.substr(location.href.search('=')+1);
+    var id = token.substr(0,token.search('\\.'));
+    var images=[];
+    var feed = new Instafeed({
+        get: 'user',
+        userId: id,
+        accessToken: token,
+        //template: '<a href="{{link}}"><img src="{{image}}" /></a>',
+        resolution: "standard_resolution",
+        sortBy: 'most-recent',
+        max_t: new Date(y,i),
+        min_t: new Date(sy,si),
+        limit: 50,
+        mock: true,
+        success: function(response) {
+            if (debug){
+                console.log('response of '+y+', '+i);
+            }
+            images = images.concat(response.data)
+            if (feed.hasNext()){
+                feed.next();
+            }else{
+                feed._sortBy(images,'likes.count',false);
+                build_pics(images)
+            }
+        },
+        error: function(){
+            console.log('no images');
+        }
+
+    });
+
+// $('#next').click(function(){
+//     feed.next();
+// });
+    feed.run();
+}
+
 function build_cal(data){
     if (debug){
         console.log('Length: '+data.length);
     }
     var images = data;
-    block = document.getElementById('instafeed');
+    var block = document.getElementById('instafeed');
     for (k = 0, len2 = images.length; k < len2; k++) {
 //          console.log('here');
         var can = document.createElement('canvas');
@@ -189,6 +248,64 @@ function build_cal(data){
         if (debug){
             $('#instafeed').append(('<br>Likes: '+images[k].likes.count));
             $('#instafeed').append('<br>Date: '+new Date(images[k].created_time*1000));
+        }
+    }
+}
+
+function build_pics(images){
+    var block = document.getElementById('instafeed')
+    var can = document.createElement('canvas')
+    block.appendChild(can)
+    add_pics_pics(can, images.slice(0,2))
+}
+
+function add_pics_pics(can, images){
+    var ctx = can.getContext('2d');
+    var url = 'JPG1/igtemplate.png';
+    var back = new Image();
+    back.src = url
+    back.onload = function(){
+        can.width = back.width
+        can.height = back.height
+        ctx.drawImage(back,0,0);
+        var frts = []
+        frts[0] = new Image()
+        frts[1] = new Image()
+        for (i = 0 ; i < 2 ; i ++){
+            var curImage = images[i]
+            if (curImage!=null){
+                imageUrl = curImage.images['standard_resolution'].url.replace('/s640x640','');
+                if (drawtext){
+                    text = curImage.caption.text;
+                }
+            }else{
+                imageUrl ='';
+            }
+            if (debug){
+                console.log('imageUrl: '+imageUrl);
+            }
+            var frt = frts[i]
+            frt.src = imageUrl
+            frt.onload = (function(i,ctx,frt,can){
+                return function(){
+                if(debug){
+                    console.log("Drawing frt_pic: ", i, frt.src)
+                    console.log("Ctx", ctx)
+                }
+                if (frt.width > frt.height) {
+                    ctx.drawImage(frt, can.width/2*(1-1300/1485)/2,can.width*(1300*(1-frt.height/frt.width)/2+200)/1485,can.width*1300/1485,can.width*1300*frt.height/frt.width/1485);
+                }else if (frt.width < frt.height) {
+                    ctx.drawImage(frt, can.width*(1485-1300+1300*(1-frt.width/frt.height))/1485/2,can.width*200/1485,can.width*1300*frt.width/frt.height/1485,can.width*1300/1485);
+                }else{
+                    if (debug){
+                        console.log("Drawing Square pics")
+                        console.log("Canvas Length and Width: ", can.width, can.height)
+                        console.log("Params: ", can.width*( (1-(1600+12)/1812)/4 + (900+12)/1812*(i) ),can.width*100/1812 ,can.width*(800/1812),can.width*800/1812)
+                    }
+                    ctx.drawImage(frt, can.width*( (1-(1600+12)/1812)/4 + (900+12)/1812*(i) ),can.width*100/1812 ,can.width*(800/1812),can.width*800/1812);
+                }
+                }
+            })(i,ctx,frt,can)
         }
     }
 }
@@ -222,7 +339,7 @@ function add_pic(can,curImage,i){
                 text = curImage.caption.text;
             }
         }else{
-            imagesUrl ='';
+            imageUrl ='';
         }
         if (debug){
             console.log('imageUrl: '+imageUrl);
